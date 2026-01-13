6642bb03da934dae2bc1b67a58af4e998d719915
Steven Leonardo <Stevenleonardo57@gmail.com>
2026-01-13 11:52:30 +0700

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt","nuxt-simple-sitemap"],
-  // @ts-ignore
-  sitemap: {
-    siteUrl: "https://prayer-schedule-two.vercel.app",
-    routes: async () => {
-      // if you have dynamic pages, generate them here
-      return [
-        "/",
-        "/about",
-        "/jadwal/jakarta", // example dynamic pages
-        "/jadwal/batam",
-      ];
-    },
-  },
+  modules: [
+    "@pinia/nuxt",
+    [
+      "nuxt-simple-sitemap",
+      {
+        siteUrl: "https://prayer-schedule-two.vercel.app",
+        routes: async () => ["/", "/about", "/jadwal/jakarta", "/jadwal/batam"],
+      },
+    ],
+  ],
+  ssr: true,
 });
