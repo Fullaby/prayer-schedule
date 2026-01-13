3752055d23a0514884e74af2b1116d07bc367fec
Steven Leonardo <Stevenleonardo57@gmail.com>
2026-01-13 11:46:07 +0700

    sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt']
-})
+  modules: ["@pinia/nuxt","nuxt-simple-sitemap"],
+  // @ts-ignore
+  sitemap: {
+    siteUrl: "https://prayer-schedule-two.vercel.app",
+    routes: async () => {
+      // if you have dynamic pages, generate them here
+      return [
+        "/",
+        "/about",
+        "/jadwal/jakarta", // example dynamic pages
+        "/jadwal/batam",
+      ];
+    },
+  },
+});
