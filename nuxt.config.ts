129e4827ac4478630cfa0c4c9dd7d4b969af1963
Steven Leonardo <Stevenleonardo57@gmail.com>
2026-01-13 11:58:02 +0700

    change icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "My Nuxt 3 App",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
+    },
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: [
